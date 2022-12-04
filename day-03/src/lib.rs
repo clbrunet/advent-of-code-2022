8366118cdb1ee32470e72c1056dbdf8c0efd1e41
clbrunet <brunet.clement5@gmail.com>
2022-12-04 16:15:19 +0100

    feat(day-03): return String instead of printing and add tests

--- day-03/src/lib.rs
+++ day-03/src/lib.rs
@@ -10,16 +10,16 @@ fn get_priority(ch: char) -> u32 {
     }
 }
 
-pub fn part_1(input: &str) {
+pub fn part_1(input: &str) -> String {
     let sum = input.lines().fold(0, |accum, line| {
         let (first, second) = line.split_at(line.len() / 2);
         let ch = first.chars().find(|&ch| second.contains(ch)).unwrap();
         accum + get_priority(ch)
     });
-    println!("Part 1 : {sum}");
+    sum.to_string()
 }
 
-pub fn part_2(input: &str) {
+pub fn part_2(input: &str) -> String {
     let sum = input.lines().array_chunks().fold(0, |accum, [a, b, c]| {
         let ch = a
             .chars()
@@ -27,5 +27,27 @@ pub fn part_2(input: &str) {
             .unwrap();
         accum + get_priority(ch)
     });
-    println!("Part 2 : {sum}");
+    sum.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    #[test]
+    fn part_1_works() {
+        assert_eq!(part_1(INPUT), "157");
+    }
+
+    #[test]
+    fn part_2_works() {
+        assert_eq!(part_2(INPUT), "70");
+    }
 }
